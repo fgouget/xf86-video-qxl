f9980eec2287a01a918ff073a45f13d9dc66a1c6
Marc-André Lureau <marcandre.lureau@gmail.com>
2012-07-04 14:01:45 -0400

    get rid of one of the gcc warnings

--- src/mspace.h
+++ src/mspace.h
@@ -9,7 +9,7 @@ extern "C" {
 
 //typedef unsigned long size_t;
 typedef void (*mspace_abort_t)(void *user_data);
-typedef void (*mspace_print_t)(void *user_data, const char *format, ...);
+typedef void (*mspace_print_t)(void *user_data, const char *format, ...)  __attribute__((format(gnu_printf, 2, 3)));
 
 void mspace_set_abort_func(mspace_abort_t f);
 void mspace_set_print_func(mspace_print_t f);
