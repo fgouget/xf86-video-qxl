4d7160c49614c9132e7b7ae4ccfda952daffeefb
Francois Gouget <fgouget@codeweavers.com>
2016-10-28 16:50:18 +0200

    spiceqxl: Remove an unused macro
    Signed-off-by: Francois Gouget <fgouget@codeweavers.com>

--- src/spiceqxl_io_port.c
+++ src/spiceqxl_io_port.c
@@ -44,17 +44,6 @@
         ret = &m_item->el;                                              \
     }
 
-#undef SPICE_RING_CONS_ITEM
-#define SPICE_RING_CONS_ITEM(r, ret) {                                  \
-        typeof(r) start = r;                                            \
-        typeof(r) end = r + 1;                                          \
-        uint32_t cons = (r)->cons & SPICE_RING_INDEX_MASK(r);           \
-        typeof(&(r)->items[cons]) m_item = &(r)->items[cons];           \
-        if (!((uint8_t*)m_item >= (uint8_t*)(start) && (uint8_t*)(m_item + 1) <= (uint8_t*)(end))) { \
-            abort();                                                    \
-        }                                                               \
-        ret = &m_item->el;                                              \
-    }
 
 static int spiceqxl_io_port_debug_level = -1;
 
